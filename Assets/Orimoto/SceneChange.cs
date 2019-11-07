3ab8a61b56b2dcbc3990c53c33fa3ad8335e57b7
o0kazuya0o <o0kazuya.orimoto0o@gmail.com>
2019-11-07 17:48:11 +0900

    シーンマネージャーの変更

--- Assets/Orimoto/SceneChange.cs
+++ Assets/Orimoto/SceneChange.cs
@@ -5,17 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
+    public enum FadeName
+    {
+        FADE_IN_DOWN,
+        FADE_IN_UP,
+        FADE_OUT_DOWN,
+        FADE_OUT_UP
+    }
+
+
+
     [SerializeField] string stageSelectSceneName;
+
+    [SerializeField] List<GameObject> fade = new List<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void ChangeScene()
@@ -23,8 +36,28 @@ public class SceneChange : MonoBehaviour
         SceneManager.LoadScene(stageSelectSceneName);
     }
 
+    
+
     public void ChangeScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void ChangeSceneFade(FadeName f)
+    {
+        switch(f)
+        {
+            case FadeName.FADE_IN_DOWN: ChangeScene();
+                break;
+            case FadeName.FADE_IN_UP:
+                ChangeScene();
+                break;
+            case FadeName.FADE_OUT_DOWN:
+                ChangeScene();
+                break;
+            case FadeName.FADE_OUT_UP:
+                ChangeScene();
+                break;
+        }
     }
 }
