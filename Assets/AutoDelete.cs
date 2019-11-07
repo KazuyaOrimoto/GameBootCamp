cff3eda8a8090dc38ec04aa8f020e0e27a80aa7e
o0kazuya0o <o0kazuya.orimoto0o@gmail.com>
2019-11-08 00:48:48 +0900

    動かないシーンを減らした

---
+++ Assets/AutoDelete.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDelete : MonoBehaviour
+{
+    float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if(time > 20.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/New Folder/BambooCreater.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BambooCreater : MonoBehaviour
+{
+    float time = 0;
+    [SerializeField] GameObject bam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if(time > 1.0f)
+        {
+            GameObject b = Instantiate(bam);
+            time = 0;
+            b.transform.position =transform.position + new Vector3(Random.Range(-50.0f,50.0f), Random.Range(-10.0f, 10.0f), Random.Range(-30.0f, 100.0f));
+        }
+    }
+}
